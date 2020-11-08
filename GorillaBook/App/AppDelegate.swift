6dedeea629f6e39001eff942389021b4a53fa796
Andres Rojas <andres.rojas@endava.com>
2020-11-08 09:42:52 -0500

    Initial project

---
+++ GorillaBook/App/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  GorillaBook
+//
+//  Created by Andres Felipe Rojas R. on 8/11/20.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+
+        window?.rootViewController = 
+
+        window?.makeKeyAndVisible()
+        return true
+    }
+}
+
